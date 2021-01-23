5d69c8c78fdc6ce714e24063785cb7fcf6f8eb29
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-01-23 13:22:43 -0300

    adicionado rotas e funçoes de request

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -4,4 +4,19 @@ const api = {}
 api.findAll = (request, response) => {
 }
 
+api.save = (request, response) => {
+}
+
+api.update = (request, response) => {
+}
+
+api.delete = (request, response) => {
+}
+
+api.findById = (request, response) => {
+}
+
+api.getAllCard = (request, response) => {
+}
+
 module.exports = api
--- nodejs/cards/src/routes/card-route.js
+++ nodejs/cards/src/routes/card-route.js
@@ -1,7 +1,16 @@
 const api = require('../controllers/card-controller')
 
 module.exports = (app) => {
-    app.route('')
+    app.route('/cards')
         .get(api.findAll)
+        .post(api.save)
+    
+    app.route('/cards/:id')
+        .put(api.update)
+        .delete(api.delete)
+        .get(api.findById)
+    
+    app.route('/cards/paginationAndSorting')
+        .get(api.getAllCard)
 
 }
