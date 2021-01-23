6821c3cff463617f7b6d929c9a637e3e3ee9ffac
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-01-23 09:58:34 -0300

    cadastro de cartão criado

---
+++ java/cards/src/main/java/com/orbitallcorp/hack21/cards/controllers/CardController.java
@@ -0,0 +1,25 @@
+package com.orbitallcorp.hack21.cards.controllers;
+
+import com.orbitallcorp.hack21.cards.domains.Card;
+import com.orbitallcorp.hack21.cards.services.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cards")
+public class CardController {
+    @Autowired
+    private CardService cardService;
+
+    @PostMapping
+    public ResponseEntity<Card> save(@RequestBody Card customer) {
+        Card savedCustomer = cardService.save((customer));
+        return new ResponseEntity(savedCustomer, HttpStatus.CREATED);
+    }
+
+}
---
+++ java/cards/src/main/java/com/orbitallcorp/hack21/cards/services/CardService.java
@@ -0,0 +1,17 @@
+package com.orbitallcorp.hack21.cards.services;
+
+import com.orbitallcorp.hack21.cards.domains.Card;
+import com.orbitallcorp.hack21.cards.repositories.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CardService {
+    @Autowired
+    private CardRepository cardRepository;
+
+    public Card save(Card card) {
+        return cardRepository.save((card));
+    }
+
+}
