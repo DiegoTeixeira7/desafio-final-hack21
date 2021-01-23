a8eb547b752292fc0c958455adfbc7bf58142517
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-01-23 13:34:20 -0300

    Funçoes para criar cartao e buscar todos os cartoes criadas

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -2,9 +2,65 @@ const neDB = require('../configurations/database')
 const api = {}
 
 api.findAll = (request, response) => {
+  neDB.find({}).exec((err, allCards) => {
+    if(err) {
+      return response.status(500).json(err);
+    } else {
+      return response.status(201).json(allCards);
+    }
+  });
 }
 
 api.save = (request, response) => {
+  const {cardNumber, embossName, customerName, 
+    documentNumber, motherName, address, city} = request.body;
+
+
+  if(!cardNumber || cardNumber < 0 ) {
+    return response.status(404).send("Número do cartão inválido!");
+  }
+
+  if(!embossName || !embossName.length) {
+    return response.status(404).send("Nome em relevo inválido!");
+  }
+
+  if(!customerName || !customerName.length) {
+    return response.status(404).send("Nome do cliente inválido!");
+  }
+
+  if(!documentNumber || documentNumber < 0 ) {
+    return response.status(404).send("Número do documento inválido!");
+  }
+
+  if(!motherName || !motherName.length) {
+    return response.status(404).send("Nome da mãe inválido!");
+  }
+
+  if(!address || !address.length) {
+    return response.status(404).send("Endereço inválido!");
+  }
+
+  if(!city || !city.length) {
+    return response.status(404).send("Cidade inválida!");
+  }
+
+  var card = {
+    cardNumber : cardNumber,
+    embossName : embossName,
+    customerName : customerName,
+    documentNumber : documentNumber,
+    motherName : motherName,
+    address : address,
+    city : city
+  };
+
+  neDB.insert(card, (err, newCard) => {
+    if(err) {
+      return response.status(500).json(err);
+    } else {
+      return response.status(201).json(newCard);
+    }
+  });
 }
 
 api.update = (request, response) => {
